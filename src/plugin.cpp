f06295c052343e6370b6cd8f0a21cdbee6f01d8d
L. Sousa <lm.sousa@fe.up.pt>
2025-05-14 16:55:30 +0100

    Implement dumper for three node types

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,78 +1,84 @@
-//===-- PrintFlangFunctionNames.cpp ---------------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// Small example Flang plugin to count/print Functions & Subroutines names.
-// It walks the Parse Tree using a Visitor struct that has Post functions for
-// FunctionStmt and SubroutineStmt to access the names of functions &
-// subroutines. It also has Pre functions for FunctionSubprogram and
-// SubroutineSubprogram so a Bool can be set to show that it is the definition
-// of a function/subroutine, and not print those that are in an Interface.
-// This plugin does not recognise Statement Functions or Module Procedures,
-// which could be dealt with through StmtFunctionStmt and MpSubprogramStmt nodes
-// respectively.
-//
-//===----------------------------------------------------------------------===//
-
 #include "flang/Frontend/FrontendActions.h"
 #include "flang/Frontend/FrontendPluginRegistry.h"
 #include "flang/Parser/dump-parse-tree.h"
+#include "flang/Parser/parse-tree.h"
 #include "flang/Parser/parsing.h"
 
 using namespace Fortran::frontend;
 
-class PrintFunctionNamesAction : public PluginParseTreeAction {
+// Visitor struct that defines Pre/Post functions for different types of nodes
+struct ParseTreeVisitor {
+public:
+  template <typename A> bool Pre(const A &) const { return true; }
+  template <typename A> void Post(const A &) const { return; }
 
-  // Visitor struct that defines Pre/Post functions for different types of nodes
-  struct ParseTreeVisitor {
-    template <typename A> bool Pre(const A &) { return true; }
-    template <typename A> void Post(const A &) const {}
+  bool Pre(const Fortran::parser::Program &v) const {
+    printf("\"%p\": {\n", &v);
+    llvm::outs() << "\"type\": \"program\",\n";
 
-    bool Pre(const Fortran::parser::FunctionSubprogram &) {
-      isInSubprogram_ = true;
-      return true;
+    const auto &lst = v.v;
+
+    llvm::outs() << "\"program-unit\": [\n";
+    for (const auto &item : lst) {
+      printf("\"%p\",\n", &item);
     }
-    void Post(const Fortran::parser::FunctionStmt &f) {
-      // if (isInSubprogram_) {
-      llvm::outs() << "Function:\t"
-                   << std::get<Fortran::parser::Name>(f.t).ToString() << "\n";
-      fcounter++;
-      isInSubprogram_ = false;
-      //}
-    }
+    llvm::outs() << "],\n";
 
-    bool Pre(const Fortran::parser::SubroutineSubprogram &) {
-      isInSubprogram_ = true;
-      return true;
-    }
-    void Post(const Fortran::parser::SubroutineStmt &s) {
-      // if (isInSubprogram_) {
-      llvm::outs() << "Subroutine:\t"
-                   << std::get<Fortran::parser::Name>(s.t).ToString() << "\n";
-      scounter++;
-      isInSubprogram_ = false;
-      //}
-    }
+    llvm::outs() << "},\n";
+    return true;
+  }
 
-    int fcounter{0};
-    int scounter{0};
+  bool Pre(const Fortran::parser::ProgramUnit &v) const {
+    printf("\"%p\": {\n", &v);
+    llvm::outs() << "\"type\": \"program-unit\",\n";
 
-  private:
-    bool isInSubprogram_{false};
-  };
+    const auto &var = v.u;
 
-  void executeAction() override {
-    ParseTreeVisitor visitor;
-    Fortran::parser::Walk(getParsing().parseTree(), visitor);
+    std::visit([](auto &arg) { printf("\"value\": \"%p\",\n", &arg.value()); },
+               var);
 
-    llvm::outs() << "\n====   Functions: " << visitor.fcounter << " ====\n";
-    llvm::outs() << "==== Subroutines: " << visitor.scounter << " ====\n";
+    llvm::outs() << "},\n";
+    return true;
+  }
+
+  bool Pre(const Fortran::parser::MainProgram &v) const {
+    printf("\"%p\": {\n", &v);
+    llvm::outs() << "\"type\": \"main-program\",\n";
+
+    const auto &[program_stmt, specification_part, execution_part,
+                 internal_subprogram_part, end_program_stmt] = v.t;
+
+    printf("\"program-stmt\": \"%p\",\n", &program_stmt.value());
+    printf("\"specification-part\": \"%p\",\n", &specification_part);
+    printf("\"execution_part\": \"%p\",\n", &execution_part);
+    printf("\"internal_subprogram_part\": \"%p\",\n",
+           &internal_subprogram_part);
+    printf("\"end-program-stmt\": \"%p\",\n", &end_program_stmt);
+
+    llvm::outs() << "},\n";
+    return true;
   }
 };
 
-const static FrontendPluginRegistry::Add<PrintFunctionNamesAction>
-    X("print-fns", "Print Function names");
+class DumpAST : public PluginParseTreeAction {
+
+  void executeAction() override {
+    llvm::outs() << "{\n";
+    ParseTreeVisitor visitor;
+    Fortran::parser::Walk(getParsing().parseTree(), visitor);
+    llvm::outs() << "}\n";
+  }
+};
+
+class DumpParseTreeAction : public PluginParseTreeAction {
+
+  void executeAction() override {
+    Fortran::parser::ParseTreeDumper visitor(llvm::outs());
+    Fortran::parser::Walk(getParsing().parseTree(), visitor);
+  }
+};
+
+const static FrontendPluginRegistry::Add<DumpAST>
+    X("dump-ast", "Dump all AST node data as a JSON object");
+const static FrontendPluginRegistry::Add<DumpParseTreeAction>
+    X2("dump-tree", "Run the ParseTreeDumper visitor on the code");
